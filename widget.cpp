abcfc040bfbaa81c422b5b4e693a398ae95c0c0a
alinaterina <alinaterina@mail.ru>
2021-11-22 10:42:54 +0300

    v1.0

---
+++ widget.cpp
@@ -0,0 +1,49 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include "TStack.h"
+#include "TCalculator.h"
+#include "TDStack.h"
+Widget::Widget(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+
+void Widget::on_pushButton_clicked()
+{
+    tmp = ui->lineEdit->text();
+
+    _tmp=tmp.toStdString();
+
+    TCalculator calc;
+
+    calc.SetExpr(_tmp);
+
+    double res = calc.calc();
+
+    ui->label_2->setText(QString::number(res));
+}
+
+
+void Widget::on_lineEdit_returnPressed()
+{
+    tmp = ui->lineEdit->text();
+
+    _tmp=tmp.toStdString();
+
+    TCalculator calc;
+
+    calc.SetExpr(_tmp);
+
+    double res = calc.calc();
+
+    ui->label_2->setText(QString::number(res));
+}
+
